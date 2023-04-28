c903ce2c70eac2db0e99ae508afaeedb6d71f621
Brendan <brendan.b517@gmail.com>
2023-04-28 00:33:14 -0400

    Passed all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        # matches = []
+        # for possible_match in anagrams:
+        #     if len(self.word) == len(possible_match) and set(self.word) == set(possible_match):
+        #         matches.append(possible_match)
+        # return matches
+        return [match for match in anagrams if len(self.word) == len(match) and set(self.word) == set(match)]
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
